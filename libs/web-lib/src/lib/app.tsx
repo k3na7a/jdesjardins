8f43b8ea48c4026a671fa057b83ff22f1096b304
John Desjardins <desjjoh@gmail.com>
2022-12-19 13:57:48 -0500

    Update app.tsx

--- libs/web-lib/src/lib/app.tsx
+++ libs/web-lib/src/lib/app.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { useAxios } from './hooks';
 import { IUser } from '@jdesjardins/dist-lib';
 import { localhost } from './apis';
-import { useReducer, useRef } from 'react';
+import { useMemo, useReducer } from 'react';
 import {
   CounterReducerActionTypes,
   counterReducer,
@@ -14,22 +14,16 @@ import { DemoContextProvider } from './context/demo.context';
 export function App() {
   const [t, i18n] = useTranslation('common', { keyPrefix: '' });
   const [state, dispatch] = useReducer(counterReducer, { value: 0 });
-
-  // const [user, setUser] = useState<IUser>()
-  const [data, loading, error] = useAxios<IUser>({
+  const [data, loading] = useAxios<IUser>({
     instance: localhost,
     config: {
       method: 'GET',
       url: '/me',
     },
   });
-  const user$ = useRef(data)
+  const user$ = useMemo<IUser | undefined>(() => data, [data]);
 
-  // function useEffect(() => {
-  //   setUser(data)
-  // }, [data])
-
-  // if (loading) return <p>loading</p>;
+  if (loading) return <p>loading</p>;
 
   // if (error) return <p>{error.message}</p>;
 
@@ -43,8 +37,8 @@ export function App() {
         >
           {state.value}
         </button>
-        {/* <NxWelcome title={user?.username || t('title', i18n)} /> */}
-        <NxWelcome title={t('title', i18n)} />
+        <NxWelcome title={user$?.username || t('title', i18n)} />
+        {/* <NxWelcome title={t('title', i18n)} /> */}
       </main>
     </DemoContextProvider>
   );
